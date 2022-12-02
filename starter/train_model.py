6ee3ae4a375283160948fbe129cd351e123c9569
cpjosejesus <cp.josejesus@gmail.com>
2022-12-01 20:26:25 -0400

    added model card

--- starter/train_model.py
+++ starter/train_model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
 import pandas as pd
-
+import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split
 from .ml.data import process_data
